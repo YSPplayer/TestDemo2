e82a2fd538c3bf604859d5e560108bbc1e9f91fd
YSPplayer <479310608@qq.com>
2026-04-05 16:33:06 +0800

    重置文件

--- engine/src/core/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod card;
-pub mod monstercard;
-
-pub use card::{Card,CardTrait};
-pub use monstercard::MonsterCard;
--- webapp/src/script/Format/xmldata.ts
+++
@@ -1,7 +0,0 @@
-export interface XmlNode {
-    parent:XmlNode | null;
-    data:any;
-    children:XmlNode[];
-    type:string;
-}
-export const filterKeys = ['body','div']
